bd42d3b4e060135c13f1e7926fa769925d93b629
Zsolt Jurányi <zsolt.juranyi@gmail.com>
2024-01-12 00:23:22 +0100

    bundle folyamat újrakonfigolva és egyszerűsítve

--- index.js
+++ index.js
@@ -3,19 +3,21 @@ import SingleInstance from 'single-instance';
 import Logger from './src/logger.js';
 import { start } from './src/main.js';
 
-try {
-	const singleInstance = new SingleInstance('dijnet-bot');
-	await singleInstance.lock();
-	start();
-} catch (error) {
+(async () => {
 	try {
-		new Logger().error(
-			error.stack ? error : 'A Díjnet bot már fut, és nem futtatható több példányban.',
-		);
-	} catch (error2) {
-		// in case Logger is also dead
-		console.log(error2);
-	}
+		const singleInstance = new SingleInstance('dijnet-bot');
+		await singleInstance.lock();
+		start();
+	} catch (error) {
+		try {
+			new Logger().error(
+				error.stack ? error : 'A Díjnet bot már fut, és nem futtatható több példányban.',
+			);
+		} catch (error2) {
+			// in case Logger is also dead
+			console.log(error2);
+		}
 
-	process.exit(1);
-}
+		process.exit(1);
+	}
+})();
